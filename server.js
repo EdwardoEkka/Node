133f9b21fabf8b8bc56c4897399f6e5b64ef49e3
Vishal Ekka <vishalekka18@gmail.com>
2024-10-28 13:19:32 +0530

    chore: Routes

--- server.js
+++ server.js
@@ -1,10 +1,39 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 // Create the server
 const server = http.createServer((req, res) => {
-  res.statusCode = 200; // Set the response status to OK
-  res.setHeader('Content-Type', 'text/html'); // Set content type
-  res.end('<p>Hello, World!</p>'); // Send the response
+  const url = req.url;
+
+  if (url === '/') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello, World!');
+  } else if (url === '/home') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Welcome, Home!');
+  } else if(url==='/my-page'){
+    const filePath = path.join(__dirname, '', 'my-page.html');
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('500 Internal Server Error');
+      } else {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        res.end(data);
+      }
+    });
+  } 
+  else {
+    // 404 Not Found for other routes
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('404 Not Found');
+  }
 });
 
 // Listen on port 3000
